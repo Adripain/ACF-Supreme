a90c5bbb8f4a9bd140b10fa64384437b7b292f1c
Adripain <41447663+Adripain@users.noreply.github.com>
2021-08-01 12:14:45 +0200

    [REMOVE] old guns

--- lua/acf/shared/guns/oldgun.lua
+++ lua/acf/shared/guns/oldgun.lua
@@ -1,14 +1,15 @@
 --define the class
-ACF_defineGunClass("OC", {
-	spread = 3.64,
-	name = "Old canon",
-	desc = "Old Canons are able to fire shells with usefull payloads from a light weight gun, at the price of limited velocities.",
-	muzzleflash = "40mm_muzzleflash_noscale",
-	rofmod = 2.5,
-	sound = "weapons/ACF_Gun/mortar_fire1.wav",
-	soundDistance = "Mortar.Fire",
-	soundNormal = " "
-} )
+
+--ACF_defineGunClass("OC", {
+--	spread = 3.64,
+--	name = "Old canon",
+--	desc = "Old Canons are able to fire shells with usefull payloads from a light weight gun, at the price of limited velocities.",
+--	muzzleflash = "40mm_muzzleflash_noscale",
+--	rofmod = 2.5,
+--	sound = "weapons/ACF_Gun/mortar_fire1.wav",
+--	soundDistance = "Mortar.Fire",
+--	soundNormal = " "
+--} )
 
 --add a gun to the class
 --ACF_defineGun("230mmOC", {
